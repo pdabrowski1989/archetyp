9fd6da360bb2082e766a6bf7584edf7e500f45c6
pdabrowski1989 <pawel.dabrowski1989@gmail.com>
2017-08-22 20:53:25 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './app/app.module.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+        filename: './dist/[name].js',
+        path: __dirname
+    }
+};
