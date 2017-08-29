8356d902bd839665b7070a1ff1bb55e9aea0e613
pdabrowski1989 <pawel.dabrowski1989@gmail.com>
2017-08-29 21:29:11 +0200

    init stable build

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,62 @@
-module.exports = require('./webpack/webpack.build.js');
+const path = require('path');
+const webpack = require('webpack');
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './app/app.module.ts'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+        filename: './dist/[name].js',
+        path: path.join(__dirname)
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(
+            {
+                warning: false,
+                mangle: true,
+                comments: false
+            }
+        ),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: {
+                baseDir: './dist'
+            }
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: {
+                    glob: './**/*.html',
+                    dot: true
+                },
+                to: './dist',
+                ignore: [
+                    {
+                        dots: true,
+                        glob: 'node_modules/**/*'
+                    },
+                    {
+                        dots: true,
+                        glob: 'dist/**/*'
+                    }
+                ]
+            }
+        ])
+    ]
+};
